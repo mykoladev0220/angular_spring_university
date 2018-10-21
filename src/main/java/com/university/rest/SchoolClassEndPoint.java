0315f0c780fa4496b7f93c5884fa9de2b8b47dd9
mykoladev0220 <mykolademchukdev@gmail.com>
2018-10-21 18:37:37 +1100

    UNIVERSITY-1 : University Back End Services

--- src/main/java/com/university/rest/SchoolClassEndPoint.java
+++ src/main/java/com/university/rest/SchoolClassEndPoint.java
@@ -2,9 +2,10 @@ package com.university.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,19 +42,21 @@ public class SchoolClassEndPoint {
     }
     
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public SchoolClass create(SchoolClass schoolClass) {
     	return schoolClassService.addSchoolClass(schoolClass);
     }    
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{className}")
     public SchoolClass edit(SchoolClass schoolClass) {
     	return schoolClassService.editSchoolClass(schoolClass);
     }
     
-    @PUT
+    @DELETE
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{className}")
     public void deleteByPk(@PathParam("className") String className) {
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,7 +8,7 @@ insert into student(last_name,first_name,age,gpa) values("Craft","Lindsey",19,2.
 insert into student(last_name,first_name,age,gpa) values("Larsen","Erica ",20,2.1);
 insert into student(last_name,first_name,age,gpa) values("Levine","Ryan",18,3.2);
 insert into student(last_name,first_name,age,gpa) values("Smith","Erik",21,1.2);
-insert into student(last_name,first_name,age,gpa) values("Sloan","Sloan",17,3.5);
+insert into student(last_name,first_name,age,gpa) values("Sloan","Jerry",17,3.5);
 insert into student(last_name,first_name,age,gpa) values("Richardson","Josh",18,2.6);
 insert into student(last_name,first_name,age,gpa) values("Olynyk","Kelly",18,2.9);
 insert into student(last_name,first_name,age,gpa) values("Clarkson","Jordan",18,3.0);
@@ -16,6 +16,8 @@ insert into student(last_name,first_name,age,gpa) values("Tatum","Jayson",19,2.7
 
 insert into school_class_student values("Biology","Duterry");
 insert into school_class_student values("Biology","Craft");
+insert into school_class_student values("Biology","Levine");
+insert into school_class_student values("Algebra","Sloan");
 insert into school_class_student values("Trigonometry","Richardson");
 insert into school_class_student values("Trigonometry","Clarkson");
 insert into school_class_student values("Trigonometry","Craft");
